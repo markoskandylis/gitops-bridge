9ea96e8287abd4f249c10be8d4941111767b0757
Markos Kandylis <markos_kandylis@hotmail.com>
2024-07-26 11:46:31 +0100

    Testing gitops v2

---
+++ argocd/iac/terraform/examples/eks/multi-cluster/hub-spoke-v2/hub-v2/variables.tf
@@ -0,0 +1,65 @@
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-west-2"
+}
+variable "kubernetes_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.30"
+}
+variable "environment" {
+  description = "Environment"
+  type        = string
+  default     = "dev"
+}
+
+variable "addons" {
+  description = "Kubernetes addons"
+  type        = any
+  default = {
+    enable_aws_load_balancer_controller = true
+    enable_aws_ebs_csi_resources        = true # generate gp2 and gp3 storage classes for ebs-csi
+    enable_metrics_server               = true
+    enable_gatekeeper                   = true
+    enable_karpenter                    = true
+    enable_argocd                       = true
+  }
+}
+# Addons Git
+variable "gitops_addons_org" {
+  description = "Git repository org/user contains for addons"
+  type        = string
+  default     = "https://github.com/markoskandylis/gitops-bridge"
+}
+variable "gitops_addons_repo" {
+  description = "Git repository contains for addons"
+  type        = string
+  default     = "gitops-bridge"
+}
+variable "gitops_addons_revision" {
+  description = "Git repository revision/branch/ref for addons"
+  type        = string
+  default     = "main"
+}
+variable "gitops_addons_basepath" {
+  description = "Git repository base path for addons"
+  type        = string
+  default     = "argocd/iac/terraform/examples/eks/multi-cluster/hub-spoke-v2/addons/"
+}
+variable "gitops_addons_path" {
+  description = "Git repository path for addons"
+  type        = string
+  default     = ""
+}
+
+variable "platform_stack_version" {
+  description = "Platform stack version"
+  type        = string
+  default     = "v1.30-v1.1.0"
+}
---
+++ argocd/iac/terraform/examples/eks/single-cluster-v2/destroy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -uo pipefail
+
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOTDIR="$(cd ${SCRIPTDIR}/../..; pwd )"
+[[ -n "${DEBUG:-}" ]] && set -x
+
+# Delete the Ingress/SVC before removing the addons
+TMPFILE=$(mktemp)
+terraform -chdir=$SCRIPTDIR output -raw configure_kubectl > "$TMPFILE"
+# check if TMPFILE contains the string "No outputs found"
+if [[ ! $(cat $TMPFILE) == *"No outputs found"* ]]; then
+  source "$TMPFILE"
+  kubectl scale deploy -n game-2048 game-2048 --replicas=0
+  kubectl scale deploy -n gatekeeper-system gatekeeper-audit --replicas=0
+  kubectl scale deploy -n gatekeeper-system gatekeeper-controller-manager --replicas=0
+  kubectl scale deploy -n kube-system metrics-server --replicas=0
+  kubectl delete -n game-2048 ing game-2048
+  kubectl delete -n argocd svc argocd-server
+  sleep 60
+fi
+
+terraform destroy -target="module.gitops_bridge_bootstrap" -auto-approve
+terraform destroy -target="module.eks_blueprints_addons" -auto-approve
+terraform destroy -target="module.eks" -auto-approve
+terraform destroy -target="module.vpc" -auto-approve
+terraform destroy -auto-approve
